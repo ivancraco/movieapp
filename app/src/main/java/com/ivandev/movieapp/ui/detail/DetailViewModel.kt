df0657c984d723b8fd088daa367cb79a2e6f58e9
IvanDev <ivancraco@hotmail.com>
2023-11-28 16:52:06 -0300

    state added

--- app/src/main/java/com/ivandev/movieapp/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/ivandev/movieapp/ui/detail/DetailViewModel.kt
@@ -1,19 +1,42 @@
 package com.ivandev.movieapp.ui.detail
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.ivandev.movieapp.domain.Repository
-import com.ivandev.movieapp.domain.model.DetailModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class DetailViewModel @Inject constructor(private val repository: Repository) : ViewModel() {
 
-    suspend fun searchMovieByID(id: Int, language: String): DetailModel? {
-        return repository.getMovieById(id, language)
+    private val _detailState = MutableStateFlow<DetailState>(DetailState.Loading)
+    val detailState: StateFlow<DetailState> = _detailState
+
+    suspend fun searchMovieByID(id: Int, language: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            _detailState.value = DetailState.Loading
+            val response = repository.getMovieById(id, language)
+            if (response != null) {
+                _detailState.value = DetailState.Success(response)
+            } else {
+                _detailState.value = DetailState.Error("Error has ocurred")
+            }
+        }
     }
 
-    suspend fun searchSerieByID(id: Int, language: String): DetailModel? {
-        return repository.getSerieById(id, language)
+    suspend fun searchSerieByID(id: Int, language: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            _detailState.value = DetailState.Loading
+            val response = repository.getSerieById(id, language)
+            if (response != null) {
+                _detailState.value = DetailState.Success(response)
+            } else {
+                _detailState.value = DetailState.Error("Error has ocurred")
+            }
+        }
     }
 }
